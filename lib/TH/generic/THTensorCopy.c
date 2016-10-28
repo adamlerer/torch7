cb541c112ee64e2657cb41a1f6e7fc710d1a6b80
Adam Lerer <alerer@fb.com>
2016-10-28 11:00:10 -0700

    WIP copy for sam

--- lib/TH/generic/THTensorCopy.c
+++ lib/TH/generic/THTensorCopy.c
@@ -2,9 +2,26 @@
 #define TH_GENERIC_FILE "generic/THTensorCopy.c"
 #else
 
-void THTensor_(copy)(THTensor *tensor, THTensor *src)
+void THTensor_(copy)(THTensor *dst, THTensor *src)
 {
-  TH_TENSOR_APPLY2(real, tensor, real, src, *tensor_data = (real)(*src_data);)
+  long srcN = THTensor_(nElement)(src);
+  long dstN = THTensor_(nElement)(dst);
+  if (srcN != dstN) {
+    THError("inconsistent tensor nElement: %ld, %ld",
+        srcN, dstN);
+  }
+  int srcContig = THTensor_(isContiguous)(src);
+  int dstContig = THTensor_(isContiguous)(dst);
+  int doMemcpy = srcContig && dstContig && (sizeof(real) < sizeof(int));
+  if (doMemcpy) {
+    real *src_data =  src->storage->data;
+    real *dst_data =  dst->storage->data;
+    if (src_data != dst_data) {
+      memcpy(dst_data, src_data, dstN * sizeof(real));
+    }
+  } else {
+    TH_TENSOR_APPLY2(real, dst, real, src, *dst_data = (real)(*src_data);)
+  }
 }
 
 #define IMPLEMENT_THTensor_COPY(TYPENAMESRC, TYPE_SRC) \
@@ -20,5 +37,6 @@ IMPLEMENT_THTensor_COPY(Int, int)
 IMPLEMENT_THTensor_COPY(Long, long)
 IMPLEMENT_THTensor_COPY(Float, float)
 IMPLEMENT_THTensor_COPY(Double, double)
+
 
 #endif
